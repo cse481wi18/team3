28ceadd2411e5f15f64005065528071ad4e57021
Kennan Mell <kmell96@gmail.com>
2018-03-12 11:24:23 -0700

    sound demo and stuff

--- applications/scripts/sound_demo.py
+++ applications/scripts/sound_demo.py
@@ -2,9 +2,14 @@
 
 import fetch_api
 import rospy
+import time
 from geometry_msgs.msg import Twist
 
-alpha = 0.5
+alpha = 0.8
+min_message_time_ms = 500
+
+def millis():
+    return int(round(time.time() * 1000))
 
 class PlanCallback(object):
 
@@ -13,6 +18,7 @@ class PlanCallback(object):
         self.prev = 0.0
         rospy.Subscriber('cmd_vel', Twist, self.callback)
         self.angular_z = 0.0
+        self.prev_message_time = 0
 
     def callback(self, msg):
         global alpha
@@ -20,17 +26,26 @@ class PlanCallback(object):
         cap = 0.48
         global sound
         if self.angular_z > cap:
+            #print "angular z: " + str(self.angular_z) + " > " + str(cap)
             if self.prev > cap and not self.playing:
-                self.playing = True
-                sound.say('left turn')
+                if (millis() - self.prev_message_time > min_message_time_ms):
+                    self.playing = True
+                    sound.say('left turn')
+                    print "angular z: " + str(self.angular_z) + " > " + str(cap) + " -> left turn"
+                    self.prev_message_time = millis()
         elif self.angular_z < -cap:
+            #print "angular z: " + str(self.angular_z) + " < " + str(-cap)
             if self.prev < -cap and not self.playing:
-                self.playing = True
-                sound.say('right turn')
+                if (millis() - self.prev_message_time > min_message_time_ms):
+                    self.playing = True
+                    sound.say('right turn')
+                    print "angular z: " + str(self.angular_z) + " < " + str(-cap) + " -> right turn"
+                    self.prev_message_time = millis()
         else:
+            #print "angular z: " + str(self.angular_z) + " -> stationary"
             self.playing = False
         self.prev = self.angular_z
-        print 'callback:', msg
+        #print 'callback:', msg
 
 rospy.init_node('speak')
 while rospy.Time().now().to_sec() == 0:
