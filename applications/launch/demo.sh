1e4ec1e071b9548fe6d2a62d77a5c579291f37c5
Kennan Mell <kmell96@gmail.com>
2018-03-11 21:57:43 -0700

    add nav_rviz

--- applications/launch/demo.sh
+++ applications/launch/demo.sh
@@ -3,7 +3,7 @@ SESSION=demo
 
 # window 1
 tmux -2 new-session -d -s $SESSION
-# tmux send-keys "roscore" C-m
+tmux send-keys "roslaunch applications nav_rviz.launch is_sim:=false" C-m
 sleep 1
 tmux new-window -t $SESSION:1 -n $SESSION
 tmux split-window -h
@@ -35,6 +35,4 @@ tmux select-pane -t 1
 tmux send-keys "python ~/robotics-ios/server_ros_bridge.py attu3.cs.washington.edu 5000" C-m
 
 #not here
-#
-#roslaunch applications nav_rviz.launch
 tmux -2 attach-session -t $SESSION
